a1c59024cce6f26de3ddc5276d77d7006f2e6d32
jialiwei <154069@163.com>
2015-01-23 10:58:09 +0800

    Update deploy-remote-main.sh

--- deploy-remote-main.sh
+++ deploy-remote-main.sh
@@ -1,17 +1,7 @@
-#host="zm_tianqi02v.add.zwt.qihoo.net zm_tianqi03v.add.zwt.qihoo.net zm_tianqi04v.add.zwt.qihoo.net zm_tianqi05v.add.zwt.qihoo.net zm_tianqi06v.add.zwt.qihoo.net zm_tianqi01v.add.bjsc.qihoo.net zm_tianqi02v.add.bjsc.qihoo.net zm_tianqi03v.add.bjsc.qihoo.net zm_tianqi04v.add.bjsc.qihoo.net"
-zwthost="zm_tianqi02v.add.zwt.qihoo.net zm_tianqi03v.add.zwt.qihoo.net zm_tianqi04v.add.zwt.qihoo.net zm_tianqi05v.add.zwt.qihoo.net"
-bjschost="zm_tianqi01v.add.bjsc.qihoo.net zm_tianqi02v.add.bjsc.qihoo.net zm_tianqi03v.add.bjsc.qihoo.net zm_tianqi04v.add.bjsc.qihoo.net"
-#host="zm_tianqi04v.add.bjsc.qihoo.net"
-for h in ${bjschost}
+host="host1 host2"
+for h in ${host}
 do
 	echo work@$h
-	rsync -avvp /home/jialiwei/tq360-prodbjsc.war work@$h:~/tq360.war
+	rsync -avvp /home/work/ROOT.war work@$h:~/ROOT.war
 	ssh work@$h 'bash -s' < deploy-remote.sh
-done
-
-for h in ${zwthost}
-do
-        echo work@$h
-        rsync -avvp /home/jialiwei/tq360-prodzwt.war work@$h:~/tq360.war
-        ssh work@$h 'bash -s' < deploy-remote.sh
 done
