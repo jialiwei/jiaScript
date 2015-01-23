0d4461e349a851bb08a6bc24ab149fc8e83edbf9
jialiwei <154069@163.com>
2015-01-23 10:59:44 +0800

    Update deploy-remote.sh

--- deploy-remote.sh
+++ deploy-remote.sh
@@ -1,25 +1,25 @@
 echo "Backing weather war..."
-cd ~/tools/tomcat-tq360
+cd ~/tools/tomcat
 mkdir /home/work/BACKUP
-\cp webapps/ROOT.war ~/BACKUP/tq360.`date +%Y-%m-%d:%H`.war
+\cp webapps/ROOT.war ~/BACKUP/ROOT.`date +%Y-%m-%d:%H`.war
 
 echo "Copying new war..."
-\cp ~/tq360.war ~/BACKUP/flycopy.tq360.`date +%Y-%m-%d`.war
+\cp ~/ROOT.war ~/BACKUP/flycopy.ROOT.`date +%Y-%m-%d`.war
 
 echo "Killing tomcat..."
-kill -9 `ps aux|grep tomcat|grep tq360|grep -v grep|awk '{print $2}'`
+kill -9 `ps aux|grep tomcat|grep -v grep|awk '{print $2}'`
 sleep 1
 
 echo "Removing root war..."
 rm webapps/ROOT* -fr
 
 echo "Copying new root war..."
-cp ~/tq360.war webapps/ROOT.war
+cp ~/ROOT.war webapps/ROOT.war
 
 sleep 1
 echo "Restarting tomcat..."
 export JAVA_HOME=/usr/local/jdk1.6
-~/tools/tomcat-tq360/bin/startup.sh
+~/tools/tomcat/bin/startup.sh
 echo "Done"
 #tail -fn100 logs/catalina.out
 
